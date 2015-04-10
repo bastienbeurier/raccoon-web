cb8f63423a3f515ab1287522df3550df8c283c39
Bastien Beurier <bastienbeurier@gmail.com>
2015-04-10 12:11:33 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raccoon::Application.config.secret_key_base = '683e54c356938dc169e3537abde432188cdcfc264b818b3c9980c16d5e4711b7e48e282387859d57239433a4b7be2a98bb2dee930200ee489156861d9f7ba190'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raccoon::Application.config.session_store :cookie_store, key: '_raccoon_session'
