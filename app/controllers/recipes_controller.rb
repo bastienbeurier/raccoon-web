892c888c684b8a96fe3b9b7095d40405746ebcfa
Bastien Beurier <bastienbeurier@gmail.com>
2015-04-11 19:56:07 -0700

    Recipe controller new and create actions.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,48 @@
 class RecipesController < ActionController::Base
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    #Create recipe.
+    @recipe = Recipe.new
+    @recipe.title = params[:title]
+    @recipe.healthiness = params[:healthiness]
+    @recipe.preparation = params[:preparation]
+    @recipe.price = params[:price]
+    @recipe.image = params[:image]
+
+    #Create ingredients.
+    ingredients = []
+
+    (1..10).each do |i|
+      unless params["ingredient_#{i}".to_sym].blank?
+        ingredient = Ingredient.new
+        ingredient.description = params["ingredient_#{i}".to_sym]
+        ingredients << ingredient
+      end
+    end
+
+    #Create steps.
+    steps = []
+
+    (1..6).each do |i|
+      unless params["step_#{i}".to_sym].blank?
+        step = Step.new
+        step.description = params["step_#{i}".to_sym]
+        steps << step
+      end
+    end
+
+    @recipe.ingredients = ingredients
+    @recipe.steps = steps
+
+    if ingredients.count > 0 and steps.count > 0 and @recipe.save
+      render action: "show"
+    else
+      @notice = 'Failed to save recipe... (no ingredient or no step)'
+      render action: "new" 
+    end
   end
 
   def update
