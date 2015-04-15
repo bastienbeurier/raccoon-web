8d03dd5668f40353b94008f50d5da40b0ec09cd3
Bastien Beurier <bastienbeurier@gmail.com>
2015-04-15 14:35:09 -0700

    Debug

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,11 +4,11 @@ class Api::V1::RecipesController < Api::V1::ApiController
     offset = params[:offset] ? params[:offset] : 0
 
     if params[:text].to_s.empty?
-      recipes = Recipe.order(created_at: :desc).limit(count).offset(offset)
+      @recipes = Recipe.order(created_at: :desc).limit(count).offset(offset)
     else
-      recipes = Recipe.where("keywords LIKE ?", "%#{params[:text]}%").order('title ASC').limit(count).offset(offset)
+      @recipes = Recipe.where("title LIKE ?", "%#{params[:text]}%").order('title ASC').limit(count).offset(offset)
     end
 
-    render json: {result: {recipes: Recipe.response(recipes)}}, status: 201
+    render json: {result: {recipes: Recipe.response(@recipes)}}, status: 201
   end
 end
